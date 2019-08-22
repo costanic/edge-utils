a7787343210309445d558a497a7a7d1aff22b5ec
Jimmy Brisson <theotherjimmy@gmail.com>
2019-08-22 09:39:45 -0500

    Add a bunch of ';'

--- debug_scripts/get_new_gw_identity/developer_gateway_identity/index.js
+++ debug_scripts/get_new_gw_identity/developer_gateway_identity/index.js
@@ -17,12 +17,12 @@
  * limitations under the License.
  */
 
-const inquirer = require('inquirer')
-const uuid = require('uuid')
-const crypto = require('crypto')
-const execSync = require('child_process').execSync
-const fs = require('fs')
-const IDGenerator = require('./IDgenerator')
+const inquirer = require('inquirer');
+const uuid = require('uuid');
+const crypto = require('crypto');
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+const IDGenerator = require('./IDgenerator');
 const program = require('commander');
 
 
@@ -34,15 +34,15 @@ program
     .option('-o, --organizationUnit []', 'Account ID')
 	.parse(process.argv);
 
-var validHostURI = /^(https\:\/\/)?((?:(?:[a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*(?:[A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9]))$/
+var validHostURI = /^(https\:\/\/)?((?:(?:[a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*(?:[A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9]))$/;
 
 function gatewayAddressQuestion(_default) {
     if (!_default || typeof _default == 'string' || _default.length < 0) {
-        _default = ''
+        _default = '';
     }
-    let message = 'Enter the gateway services API address?'
+    let message = 'Enter the gateway services API address?';
     if (_default.length > 0) {
-        message += ' ('+_default+')'
+        message += ' ('+_default+')';
     }
     return inquirer.prompt([{
         type: 'input',
@@ -50,52 +50,28 @@ function gatewayAddressQuestion(_default) {
         message: message,
         validate: function(answer) {
             if(answer.length) {
-                var m = validHostURI.exec(answer)
+                var m = validHostURI.exec(answer);
                 if (m && m.length > 2) {
                     if (typeof m[1] != 'string' || m[1].length < 1) {
-                        answer = 'https://' + m[2]
+                        answer = 'https://' + m[2];
                     }
-                    return true
+                    return true;
                 }
-                // console.log("!!!!!>",answer,'<')
-                // let success = false;
-                // if (answer.indexOf('http://') > -1) {
-                //     success = false;
-                // }
-                // if (answer.indexOf('http') > -1 && answer.slice(0, 8) !== 'https://') {
-                //     success = false;
-                // }
-                // if (answer.indexOf('https://') > -1) {
-                //     answer = answer.slice(8);
-                //     success = true
-                // }
-                // if (answer.split('.').length != 3 || answer.split('.').indexOf('') > -1) {
-                //     success = false;
-                // }
-                // console.log("11111")
-                // if(success) {
-                // // console.log("22222>",answer,"<<<")
-                //     if (/^[a-z]([a-z0-9-.]{0,62}[a-z0-9])?$/.test(answer)) {
-                // // console.log("33333")
-                //         return true;
-                //     }
-                // }
-                return 'Please enter the gateway services address?'
-
+                return 'Please enter the gateway services address?';
             } else {
-                return 'Please enter the gateway services address?'
+                return 'Please enter the gateway services address?';
             }
         }
-    }])
+    }]);
 }
 
 function apiAddressQuestion(_default) {
     if (!_default || typeof _default == 'string' || _default.length < 0) {
-        _default = ''
+        _default = '';
     }
-    let message = 'Enter API server address?'
+    let message = 'Enter API server address?';
     if (_default.length > 0) {
-        message += ' ('+_default+')'
+        message += ' ('+_default+')';
     }
     return inquirer.prompt([{
         type: 'input',
@@ -103,135 +79,116 @@ function apiAddressQuestion(_default) {
         message: message,
         validate: function(answer) {
             if(answer.length) {
-                var m = validHostURI.exec(answer)
+                var m = validHostURI.exec(answer);
                 if (m && m.length > 2) {
                     if (typeof m[1] != 'string' || m[1].length < 1) {
-                        answer = 'https://' + m[2]
+                        answer = 'https://' + m[2];
                     }
-                    return true
+                    return true;
                 }
-                // let success = false;
-                // if (answer.indexOf('http://') > -1) {
-                //     success = false;
-                // }
-                // if (answer.indexOf('http') > -1 && answer.slice(0, 8) !== 'https://') {
-                //     success = false;
-                // }
-                // if (answer.indexOf('https://') > -1) {
-                //     answer = answer.slice(8);
-                //     success = true
-                // }
-                // if (answer.split('.').length != 3 || answer.split('.').indexOf('') > -1) {
-                //     success = false;
-                // }
-                // if(success) {
-                //     if (/^[a-z]([a-z0-9-.]{0,62}[a-z0-9])?$/.test(answer)) {
-                //         return true;
-                //     }
-                // }
-                return 'Please enter API server address?'
+                return 'Please enter API server address?';
             } else {
-                return 'Please enter API server address?'
+                return 'Please enter API server address?';
             }
         }
     }])
 }
 
-var addrs = {}
+var addrs = {};
 
 var reGetBaseDomain = /https:\/\/[^\.]+\.(.*)/;
 
 var cleanupURL = function(answer) {
-    var m = validHostURI.exec(answer)
+    var m = validHostURI.exec(answer);
     if (m && m.length > 2) {
         if (typeof m[1] != 'string' || m[1].length < 1) {
-            answer = 'https://' + m[2]
+            answer = 'https://' + m[2];
         }
-        return answer
+        return answer;
     }    
 }
 
 async function confirmAddresses() {
 
-    let ok = {confirmed: false}
-    var ret = {}
+    let ok = {confirmed: false};
+    var ret = {};
     while (!ok.confirmed) {
-        addrs.apiAddr = await apiAddressQuestion()
-        addrs.gwAddr = await gatewayAddressQuestion()
-        ret.gatewayServicesAddress = cleanupURL(addrs.gwAddr.gatewayServicesAddress)
-        ret.apiServerAddress = cleanupURL(addrs.apiAddr.apiServerAddress)
-        console.log("API address: %s\nGateway services API addresss: %s\n",ret.apiServerAddress,ret.gatewayServicesAddress)
+        addrs.apiAddr = await apiAddressQuestion();
+        addrs.gwAddr = await gatewayAddressQuestion();
+        ret.gatewayServicesAddress = cleanupURL(addrs.gwAddr.gatewayServicesAddress);
+        ret.apiServerAddress = cleanupURL(addrs.apiAddr.apiServerAddress);
+        console.log("API address: %s\nGateway services API addresss: %s\n",ret.apiServerAddress,ret.gatewayServicesAddress);
         ok = await inquirer.prompt([{
             type: 'confirm',
             name: 'confirmed',
             message: 'Confirm these are the correct addresses.',
             validate: function(answer) {
-                console.log("HEY!!!")
+                console.log("HEY!!!");
                 if (answer == 'y' || answer == 'Y') {
-                    ok = true
-                    return true
+                    ok = true;
+                    return true;
                 }
             }
-        }])
+        }]);
     }
-    return ret
+    return ret;
 }
 
 function generateRandomEUI() {
-    return [crypto.randomBytes(1)[0], crypto.randomBytes(1)[0], crypto.randomBytes(1)[0]]
+    return [crypto.randomBytes(1)[0], crypto.randomBytes(1)[0], crypto.randomBytes(1)[0]];
 }
 
 const run = async() => {
-    let identity_obj = {}
+    let identity_obj = {};
 
     let currentSerialNumber = crypto.randomBytes(2).readUInt16BE(0, true);
-    identity_obj.serialNumber = IDGenerator.SerialIDGenerator(program.serialNumberPrefix || 'SOFT', currentSerialNumber, currentSerialNumber + 1)
-    identity_obj.OU = program.organizationUnit || uuid.v4().replace(/-/g, "")
-    identity_obj.deviceID = uuid.v4().replace(/-/g, "")
-    identity_obj.hardwareVersion = "rpi3bplus"
-    identity_obj.radioConfig = "00"
-    identity_obj.ledConfig = "01"
-    identity_obj.category = "development"
-    let eui = generateRandomEUI()
-    identity_obj.ethernetMAC = [0, 165, 9].concat(eui)
-    identity_obj.sixBMAC = [0, 165, 9, 0, 1].concat(eui)
+    identity_obj.serialNumber = IDGenerator.SerialIDGenerator(program.serialNumberPrefix || 'SOFT', currentSerialNumber, currentSerialNumber + 1);
+    identity_obj.OU = program.organizationUnit || uuid.v4().replace(/-/g, "");
+    identity_obj.deviceID = uuid.v4().replace(/-/g, "");
+    identity_obj.hardwareVersion = "rpi3bplus";
+    identity_obj.radioConfig = "00";
+    identity_obj.ledConfig = "01";
+    identity_obj.category = "development";
+    let eui = generateRandomEUI();
+    identity_obj.ethernetMAC = [0, 165, 9].concat(eui);
+    identity_obj.sixBMAC = [0, 165, 9, 0, 1].concat(eui);
     identity_obj.hash = [];
 
     if(!program.gatewayServicesAddress)
-        addrs = await confirmAddresses()
+        addrs = await confirmAddresses();
     else
         addrs = {
             gatewayServicesAddress: program.gatewayServicesAddress
-        }
+        };
 
 //    identity_obj = Object.assign({}, identity_obj, await gatewayAddressQuestion(), await apiAddressQuestion())
-    identity_obj = Object.assign({}, identity_obj, addrs)
+    identity_obj = Object.assign({}, identity_obj, addrs);
     identity_obj.cloudAddress = identity_obj.gatewayServicesAddress;
 
-    identity_obj.ssl = {}
-    execSync('OU=' + identity_obj.OU + ' internalid=' + identity_obj.deviceID + ' ./generate_self_signed_certs.sh')
-    const device_key = fs.readFileSync('./temp_certs/device_private_key.pem', 'utf8')
-	const device_cert = fs.readFileSync('./temp_certs/device_cert.pem', 'utf8')
-	const root_cert = fs.readFileSync('./temp_certs/root_cert.pem', 'utf8')
-	const intermediate_cert = fs.readFileSync('./temp_certs/intermediate_cert.pem', 'utf8')
+    identity_obj.ssl = {};
+    execSync('OU=' + identity_obj.OU + ' internalid=' + identity_obj.deviceID + ' ./generate_self_signed_certs.sh');
+    const device_key = fs.readFileSync('./temp_certs/device_private_key.pem', 'utf8');
+    const device_cert = fs.readFileSync('./temp_certs/device_cert.pem', 'utf8');
+    const root_cert = fs.readFileSync('./temp_certs/root_cert.pem', 'utf8');
+    const intermediate_cert = fs.readFileSync('./temp_certs/intermediate_cert.pem', 'utf8');
 
-    identity_obj.ssl.client = {}
-    identity_obj.ssl.client.key = device_key
-    identity_obj.ssl.client.certificate = device_cert
+    identity_obj.ssl.client = {};
+    identity_obj.ssl.client.key = device_key;
+    identity_obj.ssl.client.certificate = device_cert;
 
-    identity_obj.ssl.server = {}
-	identity_obj.ssl.server.key = device_key
-    identity_obj.ssl.server.certificate = device_cert
+    identity_obj.ssl.server = {};
+    identity_obj.ssl.server.key = device_key;
+    identity_obj.ssl.server.certificate = device_cert;
 
-    identity_obj.ssl.ca = {}
-	identity_obj.ssl.ca.ca = root_cert
-    identity_obj.ssl.ca.intermediate = intermediate_cert
+    identity_obj.ssl.ca = {};
+    identity_obj.ssl.ca.ca = root_cert;
+    identity_obj.ssl.ca.intermediate = intermediate_cert;
 
-    execSync('rm -rf ./temp_certs')
+    execSync('rm -rf ./temp_certs');
 
-    console.log('Writing developer identity file with serialNumber=%s, identity.json', identity_obj.serialNumber)
-    fs.writeFileSync('./identity.json', JSON.stringify(identity_obj, null, 4), 'utf8')
-    console.log('Success. Bye!')
+    console.log('Writing developer identity file with serialNumber=%s, identity.json', identity_obj.serialNumber);
+    fs.writeFileSync('./identity.json', JSON.stringify(identity_obj, null, 4), 'utf8');
+    console.log('Success. Bye!');
 }
 
-run()
+run();
